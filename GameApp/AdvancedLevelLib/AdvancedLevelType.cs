edc5efa927a308e7407fc5270310c02a388425fd
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-07-07 15:57:34 +0530

    updatedlibrary files

---
+++ GameApp/AdvancedLevelLib/AdvancedLevelType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedLevelLib
+{
+    public class AdvancedLevelType
+    {
+        public void Play(string userName, int creaditPoints)
+        {
+            string message = $"game started {userName} Level:Advanced {creaditPoints}";
+        }
+    }
+}
---
+++ GameApp/BasicLevelLib/BasicLevelType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLevelLib
+{
+    public class BasicLevelType
+    {
+        public void Play(string userName, int creaditPoints)
+        {
+
+         string  message = $"game started {userName} Level:Basic {creaditPoints}" ;
+        }
+
+    }
+}
